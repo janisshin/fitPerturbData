594831354e833a1e4c7085e322f1d897eb53922f
janisshin <janiseshin@gmail.com>
2021-09-01 00:55:12 -0700

    data and processing

--- genAlgo/runGenAlgo.py
+++ genAlgo/runGenAlgo.py
@@ -1,9 +1,11 @@
-# for MMARL runs
-
-import genAlgo_MMARL
+from core import genAlgo_MMARL
 import tellurium as te
-import models
+from core import models
 
-folderName = models.generateModelFiles(100, groundTruthModel_string=models.groundTruth_mod_e, parameters=models.K_LIST)
+runID = models.makeFolder('data/',date = True)
+# populationFolder = models.generateModelFiles(100, groundTruthModel_string=models.groundTruth_mod_e, parameters=models.K_LIST, folderName=runID)
 # folderName = 'genAlgo_population'
-genAlgo_MMARL.runGeneticAlgorithm(folderName, survivorRatio=(10,90), lastGeneration=300, tolerance=0.1)
+# genAlgo_MMARL.runGeneticAlgorithm(populationFolder, survivorRatio=(10,90), lastGeneration=300, tolerance=0.01, runID=runID)
+
+populationFolder = models.generateModelFiles(100, groundTruthModel_string=models.groundTruth_MM_e, parameters=models.Km_LIST)
+genAlgo_MMARL.runGeneticAlgorithm(populationFolder, groundTruth=models.groundTruth_MM_e, parameters=models.Km_LIST, survivorRatio=(10,90), lastGeneration=300, tolerance=0.01, runID=runID)
