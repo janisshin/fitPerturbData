053a900b363183bde2a4fba60958f31500b187f1
janisshin <janiseshin@gmail.com>
2021-09-16 00:19:15 -0700

    general variables

--- genAlgo/backCheck.py
+++ genAlgo/backCheck.py
@@ -5,7 +5,7 @@ import numpy as np
 from core import models
 from core import evaluate
 
-def getFoldChangeValues(testModel=te.loada(models.groundTruth_mod_e), paramData="paramData.list", folderName='data'):
+def getFoldChangeValues(testModel=te.loada(models.groundTruth_mod_e), parameters=models.K_LIST, paramData="paramData.list", n_individuals=100):
     """
     Parameters
 
@@ -25,20 +25,15 @@ def getFoldChangeValues(testModel=te.loada(models.groundTruth_mod_e), paramData=
     # reshape into numpy
     arr = np.array(list_of_lines)
     arr = arr.astype('float64')
-    arr = np.reshape(arr, (100, 5)) # should replace these numbers with variables
+    # (number of models, number of parameters per model)
+    arr = np.reshape(arr, (n_individuals, len(parameters))) 
     
-
-    # for each row in array
-        # set the test model's parameters to the 5 parameters
-        # runExperiment
-        # print allData
-
     # open file
-    f = open(folderName + "/allData.list", "w")
+    f = open("data/allData.list", "w")
 
     for row in arr:
         for i, parameter in enumerate(row):
-            testModel.setValue(models.K_LIST[i], parameter) 
+            testModel.setValue(parameters[i], parameter) 
         alteredModel = testModel.getCurrentAntimony()
         allData = evaluate.runExperiment(alteredModel).tolist()
         s = " ".join(str(datum) for datum in allData)
