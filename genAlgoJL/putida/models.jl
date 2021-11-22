987cfb845307b6e01a7260f7bf3a621d7376a471
janisshin <janiseshin@gmail.com>
2021-11-22 12:20:08 -0800

    change groundtruth string to template

--- genAlgoJL/putida/models.jl
+++ genAlgoJL/putida/models.jl
@@ -63,8 +63,7 @@ K_LIST_putida = ["Km","Km1","Km3","Km5","Km7","Km9","Km11","Km13","Km15","Km17",
 ENZYMES = ["e1", "e2", "e3", "e4", "e5"]
 ENZYMES_putida = ["e1","e2","e3","e4","e5","e6","e7","e8","e9","e10","e11","e12","e13","e14","e15","e16","e17","e18","e19","e20","e21","e22","e23","e25","e26","e27","e28","e29","e30","e31","e32","e33","e34","e35","e36","e37","e38","e39","e40","e41","e42","e43","e44","e45","e46","e47","e48","e49","e50","e51","e52","e53","e54","e55","e56","e57","e58","e59","e60","e61","e62","e63","e64","e66","e67","e68","e69","e70","e71","e73","e74","e75","e76","e77","e78","e79","e80","e81"]
 
-function generateModelFiles(n, folderName, groundTruthModel_string=groundTruth_e, parameters=K_LIST)
-    print(groundTruthModel_string)
+function generateModelFiles(n, folderName, groundTruthModel_File=nothing, parameters=K_LIST)
     #=
     generates n number of models that do not contain Keq terms
         
@@ -81,9 +80,12 @@ function generateModelFiles(n, folderName, groundTruthModel_string=groundTruth_e
     folderName = makeFolder(folderName * "/population")
     rr = RoadRunner.createRRInstance()
 
+    io = open(groundTruthModel_File)
+    template = read(io, String)
+    close(io)
+
     for number in 1:n
-        
-        randomModel = RoadRunner.loadSBML(rr, groundTruthModel_string) # make a copy of the model ##############
+        randomModel = RoadRunner.loadSBML(rr, template) 
         # set k values to random numbers 
         for p in parameters
             pValue = rand(Uniform(0, 1))
