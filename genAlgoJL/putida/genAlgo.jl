b2bbe0002842c5e50d49122b5b374b651023a123
janisshin <janiseshin@gmail.com>
2021-11-24 15:27:30 -0800

    filename vs string error

--- genAlgoJL/putida/genAlgo.jl
+++ genAlgoJL/putida/genAlgo.jl
@@ -118,9 +118,10 @@ function calculateFitness(population, groundTruth)
     groundTruthData = Main.Evaluate.runExperiment(groundTruth) #, omit=omission
     for individual in readdir(population)
         print(individual) #################################################
-        io = open(population * "/" * individual, "r")
-        individualData = Main.Evaluate.runExperiment(read(io, String))# , omit=omission)
-        close(io)
+        # io = open(population * "/" * individual, "r")
+        # individualData = Main.Evaluate.runExperiment(read(io, String))# , omit=omission)
+        # close(io)
+        individualData = Main.Evaluate.runExperiment(population * "/" * individual)# , omit=omission)
     
         chiSq = sum((groundTruthData - individualData).^2)
         scores[individual] = chiSq
